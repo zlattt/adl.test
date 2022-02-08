750b820a2eca9c2a36aac897c2be9ef0941f0d3f
Elias Gold <bonojibonoji@gmail.com>
2022-02-08 20:10:59 +0300

    add font preload, gatsby-ssr api

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,8 @@
+const React = require("react")
+/*const fontUrl1 = require("/src/fonts/esqaderoffcy4f.woff2")*/
+
+/*todo(maybe): make an array of fonts and map them in onPreRenderHtml function, maybe more handful to add/delete fonts
+
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
@@ -5,3 +10,9 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
+  const headComponents = getHeadComponents()
+  headComponents.unshift(<link key="font1" rel="preload" href={require("/src/fonts/esqaderoffcy4f.woff2").default} as="font" type="font/woff2" crossOrigin="" />)
+  replaceHeadComponents(headComponents)
+}
