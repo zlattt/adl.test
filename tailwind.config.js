35b9f0cd06fa6e82e87ea5e1f487631473121534
Elias Gold <bonojibonoji@gmail.com>
2022-02-08 15:37:45 +0300

    add esqadero font and tailwind shortcut

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        esqadero: ["'Esqadero FF CY 4F'", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 }
