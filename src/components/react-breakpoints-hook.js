5940499285df919e915b89a1d215f87753a79052
Elias Gold <bonojibonoji@gmail.com>
2022-04-09 00:25:45 +0300

    use of browser-monads

--- src/components/react-breakpoints-hook.js
+++ src/components/react-breakpoints-hook.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-//import { window, document } from "browser-monads";
+import { window, document } from "browser-monads";
 
 const getWidth = () => window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
